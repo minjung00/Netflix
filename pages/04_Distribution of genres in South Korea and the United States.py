d04daaaf4c69773920688684194b3f02f956ef05
Minjung Kang <135298407+minjung00@users.noreply.github.com>
2023-06-27 15:31:18 +0900

    Update 04_Distribution of genres in South Korea and the United States.py

--- pages/04_Distribution of genres in South Korea and the United States.py
+++ pages/04_Distribution of genres in South Korea and the United States.py
@@ -17,26 +17,33 @@ usa_data = data[data['country'] == 'United States']
 
 data['main_genre'] = data['listed_in'].str.split(',').str[0]
 
-genre_counts = sk_data['listed_in'].str.split(',').explode().str.strip().value_counts()
-genre_counts = data['listed_in'].str.split(',').explode().str.strip().value_counts()
+sk_genre_counts = sk_data['listed_in'].str.split(',').explode().str.strip().value_counts()
 
-genre_table = pd.DataFrame({'Genre': genre_counts.index, 'Count': genre_counts})
-top_10_genres = genre_table.head(10)
+# DataFrame 생성
+genre_table = pd.DataFrame({'Genre': sk_genre_counts.index, 'Count': sk_genre_counts})
+
+sk_top_10_genres = genre_table.head(10)
+
+usa_genre_counts = data['listed_in'].str.split(',').explode().str.strip().value_counts()
+
+# DataFrame 생성
+genre_table = pd.DataFrame({'Genre': usa_genre_counts.index, 'Count': usa_genre_counts})
+
+usa_top_10_genres = genre_table.head(10)
 
 with tab1:
-  #파이 차트
-  plt.pie(top_10_genres.Count, labels=top_10_genres.index, autopct='%1.1f%%')
-    
+  plt.pie(sk_top_10_genres.Count, labels=sk_top_10_genres.index, autopct='%1.1f%%')
+
   #차트 제목
   plt.title('Netflix Shows in the South Korea')
-    
+
   #출력
   plt.show()
   st.pyplot(plt)
 
 with tab2:
   #파이 차트
-  plt.pie(top_10_genres.Count, labels=top_10_genres.index, autopct='%1.1f%%')
+  plt.pie(usa_top_10_genres.Count, labels=usa_top_10_genres.index, autopct='%1.1f%%')
 
   #차트 제목
   plt.title('Netflix Shows in the United States')
