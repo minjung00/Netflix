ef3bd4f6f7207202fbf4e2cd04719f70347f326f
Minjung Kang <135298407+minjung00@users.noreply.github.com>
2023-06-27 15:46:53 +0900

    Update 02_Number of Movies and TV shows in South Korea and the United States.py

--- pages/02_Number of Movies and TV shows in South Korea and the United States.py
+++ pages/02_Number of Movies and TV shows in South Korea and the United States.py
@@ -18,8 +18,8 @@ with tab1:
             wedgeprops={'edgecolor': 'white', 'width': 0.7}, colors=colors)
     plt.axis('equal')
     plt.title('Netflix Shows in South Korea')
-    plt.show()
     st.pyplot(plt)
+    plt.show()
 with tab2:
     #usa_data_counts = usa_data['type'].value_counts()
     colors = ['green', 'mistyrose']
